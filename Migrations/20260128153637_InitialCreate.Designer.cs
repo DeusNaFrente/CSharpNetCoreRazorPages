3b104a3faeeac0fdfff3667eb05e7ed5281f51fe
Junior <94642224+DeusNaFrente@users.noreply.github.com>
2026-01-29 11:40:34 -0300

    Migrate Inicial

--- Migrations/20260128153637_InitialCreate.Designer.cs
+++ Migrations/20260128153637_InitialCreate.Designer.cs
@@ -6,7 +6,7 @@ using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
-#nullable disable
+#nullable disable 
 
 namespace ContatosApp.Migrations
 {
