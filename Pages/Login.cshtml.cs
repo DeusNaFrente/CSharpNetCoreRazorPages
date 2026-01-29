1e2555e31b54fe48ac36f154e2584624347d7a3a
Junior <94642224+DeusNaFrente@users.noreply.github.com>
2026-01-29 11:57:05 -0300

    Lógica de login

--- Pages/Login.cshtml.cs
+++ Pages/Login.cshtml.cs
@@ -1,4 +1,4 @@
-using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations; 
 using System.Security.Claims;
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authentication.Cookies;
@@ -63,3 +63,4 @@ public class LoginModel : PageModel
         public string Password { get; set; } = string.Empty;
     }
 }
+
